d1a2a748bac42418afc96b09f1317d72ac091755
Fabrice Chaplain <75309565+Akrom4@users.noreply.github.com>
2024-03-15 16:45:56 +0100

    "chart"

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,130 +362,6 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
-export interface ApiFavoriteFavorite extends Schema.CollectionType {
-  collectionName: 'favorites';
-  info: {
-    singularName: 'favorite';
-    pluralName: 'favorites';
-    displayName: 'Favorite';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    users_permissions_user: Attribute.Relation<
-      'api::favorite.favorite',
-      'manyToOne',
-      'plugin::users-permissions.user'
-    >;
-    openings: Attribute.Relation<
-      'api::favorite.favorite',
-      'manyToMany',
-      'api::opening.opening'
-    >;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::favorite.favorite',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::favorite.favorite',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
-export interface ApiOpeningOpening extends Schema.CollectionType {
-  collectionName: 'openings';
-  info: {
-    singularName: 'opening';
-    pluralName: 'openings';
-    displayName: 'Opening';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    Name: Attribute.String & Attribute.Required;
-    Link: Attribute.String;
-    Image: Attribute.Media;
-    player: Attribute.Relation<
-      'api::opening.opening',
-      'manyToOne',
-      'api::player.player'
-    >;
-    Description: Attribute.RichText;
-    favorites: Attribute.Relation<
-      'api::opening.opening',
-      'manyToMany',
-      'api::favorite.favorite'
-    >;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::opening.opening',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::opening.opening',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
-export interface ApiPlayerPlayer extends Schema.CollectionType {
-  collectionName: 'players';
-  info: {
-    singularName: 'player';
-    pluralName: 'players';
-    displayName: 'Player';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    Lastname: Attribute.String & Attribute.Required;
-    Firstname: Attribute.String;
-    Birthdate: Attribute.Date;
-    Picture: Attribute.Media;
-    Biography: Attribute.Text;
-    openings: Attribute.Relation<
-      'api::player.player',
-      'oneToMany',
-      'api::opening.opening'
-    >;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::player.player',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::player.player',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -714,6 +590,53 @@ export interface PluginContentReleasesReleaseAction
   };
 }
 
+export interface PluginI18NLocale extends Schema.CollectionType {
+  collectionName: 'i18n_locale';
+  info: {
+    singularName: 'locale';
+    pluralName: 'locales';
+    collectionName: 'locales';
+    displayName: 'Locale';
+    description: '';
+  };
+  options: {
+    draftAndPublish: false;
+  };
+  pluginOptions: {
+    'content-manager': {
+      visible: false;
+    };
+    'content-type-builder': {
+      visible: false;
+    };
+  };
+  attributes: {
+    name: Attribute.String &
+      Attribute.SetMinMax<
+        {
+          min: 1;
+          max: 50;
+        },
+        number
+      >;
+    code: Attribute.String & Attribute.Unique;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUsersPermissionsPermission
   extends Schema.CollectionType {
   collectionName: 'up_permissions';
@@ -870,46 +793,123 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
 }
 
-export interface PluginI18NLocale extends Schema.CollectionType {
-  collectionName: 'i18n_locale';
+export interface ApiFavoriteFavorite extends Schema.CollectionType {
+  collectionName: 'favorites';
   info: {
-    singularName: 'locale';
-    pluralName: 'locales';
-    collectionName: 'locales';
-    displayName: 'Locale';
+    singularName: 'favorite';
+    pluralName: 'favorites';
+    displayName: 'Favorite';
     description: '';
   };
   options: {
-    draftAndPublish: false;
-  };
-  pluginOptions: {
-    'content-manager': {
-      visible: false;
-    };
-    'content-type-builder': {
-      visible: false;
-    };
+    draftAndPublish: true;
   };
   attributes: {
-    name: Attribute.String &
-      Attribute.SetMinMax<
-        {
-          min: 1;
-          max: 50;
-        },
-        number
-      >;
-    code: Attribute.String & Attribute.Unique;
+    users_permissions_user: Attribute.Relation<
+      'api::favorite.favorite',
+      'manyToOne',
+      'plugin::users-permissions.user'
+    >;
+    openings: Attribute.Relation<
+      'api::favorite.favorite',
+      'manyToMany',
+      'api::opening.opening'
+    >;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
-      'plugin::i18n.locale',
+      'api::favorite.favorite',
       'oneToOne',
       'admin::user'
     > &
       Attribute.Private;
     updatedBy: Attribute.Relation<
-      'plugin::i18n.locale',
+      'api::favorite.favorite',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiOpeningOpening extends Schema.CollectionType {
+  collectionName: 'openings';
+  info: {
+    singularName: 'opening';
+    pluralName: 'openings';
+    displayName: 'Opening';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    Name: Attribute.String & Attribute.Required;
+    Link: Attribute.String;
+    Image: Attribute.Media;
+    player: Attribute.Relation<
+      'api::opening.opening',
+      'manyToOne',
+      'api::player.player'
+    >;
+    Description: Attribute.RichText;
+    favorites: Attribute.Relation<
+      'api::opening.opening',
+      'manyToMany',
+      'api::favorite.favorite'
+    >;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::opening.opening',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::opening.opening',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiPlayerPlayer extends Schema.CollectionType {
+  collectionName: 'players';
+  info: {
+    singularName: 'player';
+    pluralName: 'players';
+    displayName: 'Player';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    Lastname: Attribute.String & Attribute.Required;
+    Firstname: Attribute.String;
+    Birthdate: Attribute.Date;
+    Picture: Attribute.Media;
+    Biography: Attribute.Text;
+    openings: Attribute.Relation<
+      'api::player.player',
+      'oneToMany',
+      'api::opening.opening'
+    >;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::player.player',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::player.player',
       'oneToOne',
       'admin::user'
     > &
@@ -927,17 +927,17 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
-      'api::favorite.favorite': ApiFavoriteFavorite;
-      'api::opening.opening': ApiOpeningOpening;
-      'api::player.player': ApiPlayerPlayer;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
+      'plugin::i18n.locale': PluginI18NLocale;
       'plugin::users-permissions.permission': PluginUsersPermissionsPermission;
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
-      'plugin::i18n.locale': PluginI18NLocale;
+      'api::favorite.favorite': ApiFavoriteFavorite;
+      'api::opening.opening': ApiOpeningOpening;
+      'api::player.player': ApiPlayerPlayer;
     }
   }
 }
